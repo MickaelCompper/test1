258cf83f3f3c1b2dc38607b222f8cf90f64f264d
Mickaël COMPPER <denturesdaemon@protonmail.com>
2022-08-02 15:57:29 +0200

    H2

--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <!doctype html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <title>Intro git</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <title>Intro git</title>
+</head>
 
-    <body>
-        <h1>Tuto git et github</h1>
+<body>
+    <h1>Tuto git et github</h1>
 
-        <h2>Liste des paragraphes</h2>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro totam at nostrum magnam nihil officiis vero iste qui quasi eum! Porro, eius illo quisquam iusto facere aperiam cumque consequatur nostrum!</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, ipsam optio quae maxime nesciunt quas repellat at aliquam quam dignissimos. Saepe aspernatur minima nesciunt nulla incidunt deleniti labore accusamus cupiditate!</p>
-        <p>Ipsum, est, suscipit, perspiciatis, hic deserunt laboriosam molestias sint sit quibusdam officiis id tempora in eaque nobis cum aspernatur doloremque fuga expedita porro nisi. Excepturi odit adipisci impedit voluptatum molestias!</p>
-        <p>Ipsum, est, suscipit, perspiciatis, hic deserunt laboriosam molestias sint sit quibusdam officiis id tempora in eaque nobis cum aspernatur doloremque fuga expedita porro nisi. Excepturi odit adipisci impedit voluptatum molestias!</p>
-        <p>Eius, reprehenderit, tempora deserunt dolor itaque eveniet cumque magnam nam sit molestiae atque consectetur perspiciatis praesentium laborum alias nobis tenetur ex incidunt! Porro vero itaque doloremque enim rerum numquam nisi.</p>
-    </body>
+    <h2>Liste des paragraphes</h2>
+    <h3>H3</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro totam at nostrum magnam nihil officiis vero iste qui quasi eum! Porro, eius illo quisquam iusto facere aperiam cumque consequatur nostrum!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, ipsam optio quae maxime nesciunt quas repellat at aliquam quam dignissimos. Saepe aspernatur minima nesciunt nulla incidunt deleniti labore accusamus cupiditate!</p>
+    <p>Ipsum, est, suscipit, perspiciatis, hic deserunt laboriosam molestias sint sit quibusdam officiis id tempora in eaque nobis cum aspernatur doloremque fuga expedita porro nisi. Excepturi odit adipisci impedit voluptatum molestias!</p>
+    <p>Ipsum, est, suscipit, perspiciatis, hic deserunt laboriosam molestias sint sit quibusdam officiis id tempora in eaque nobis cum aspernatur doloremque fuga expedita porro nisi. Excepturi odit adipisci impedit voluptatum molestias!</p>
+    <p>Eius, reprehenderit, tempora deserunt dolor itaque eveniet cumque magnam nam sit molestiae atque consectetur perspiciatis praesentium laborum alias nobis tenetur ex incidunt! Porro vero itaque doloremque enim rerum numquam nisi.</p>
+</body>
 
 </html>
