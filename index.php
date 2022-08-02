bf4ede60d276621344507fa8a206b14e2dd6905f
Rodland MANDOMBI <rodlandarmageddon@gmail.com>
2022-08-02 15:17:14 +0200

    modif index

--- index.php
+++ index.php
@@ -7,13 +7,14 @@
     </head>
 
     <body>
-        <h1>Introduction à git et github</h1>
+        <h1>Tuto git et github</h1>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, numquam voluptas provident corporis quidem qui aliquid temporibus maxime nostrum. Eligendi, corporis, ullam officiis ipsa inventore odio voluptates harum commodi ea!</p>
-        <p>Ut, asperiores officiis voluptates laboriosam iusto nam voluptate doloremque quas deserunt dolores eos adipisci totam nisi modi quaerat neque similique qui veritatis repellat aperiam quisquam quo consectetur recusandae iste aliquam.</p>
+        <h2>Liste des paragraphes</h2>
+
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, ipsam optio quae maxime nesciunt quas repellat at aliquam quam dignissimos. Saepe aspernatur minima nesciunt nulla incidunt deleniti labore accusamus cupiditate!</p>
         <p>Ipsum, est, suscipit, perspiciatis, hic deserunt laboriosam molestias sint sit quibusdam officiis id tempora in eaque nobis cum aspernatur doloremque fuga expedita porro nisi. Excepturi odit adipisci impedit voluptatum molestias!</p>
         <p>Eius, reprehenderit, tempora deserunt dolor itaque eveniet cumque magnam nam sit molestiae atque consectetur perspiciatis praesentium laborum alias nobis tenetur ex incidunt! Porro vero itaque doloremque enim rerum numquam nisi.</p>
-    </body> 
+    </body>
 
 </html>
