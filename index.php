f5c0a6d07dea58e04c876a4927a30b487e4577b0
Mickaël COMPPER <denturesdaemon@protonmail.com>
2022-08-02 14:34:28 +0200

    index3

--- index.php
+++ index.php
@@ -7,8 +7,10 @@
     </head>
 
     <body>
-        <h1>Intro git</h1>
+        <h1>Introduction à git et github</h1>
 
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, numquam voluptas provident corporis quidem qui aliquid temporibus maxime nostrum. Eligendi, corporis, ullam officiis ipsa inventore odio voluptates harum commodi ea!</p>
+        <p>Ut, asperiores officiis voluptates laboriosam iusto nam voluptate doloremque quas deserunt dolores eos adipisci totam nisi modi quaerat neque similique qui veritatis repellat aperiam quisquam quo consectetur recusandae iste aliquam.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
         <p>Ipsum, est, suscipit, perspiciatis, hic deserunt laboriosam molestias sint sit quibusdam officiis id tempora in eaque nobis cum aspernatur doloremque fuga expedita porro nisi. Excepturi odit adipisci impedit voluptatum molestias!</p>
         <p>Eius, reprehenderit, tempora deserunt dolor itaque eveniet cumque magnam nam sit molestiae atque consectetur perspiciatis praesentium laborum alias nobis tenetur ex incidunt! Porro vero itaque doloremque enim rerum numquam nisi.</p>
