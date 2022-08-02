cad3f742017428c4676b9d20218bc262df5c759e
Mickaël COMPPER <denturesdaemon@protonmail.com>
2022-08-02 14:54:58 +0200

    index ajout

--- index.php
+++ index.php
@@ -14,6 +14,6 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta fugiat facere quae quo. Pariatur, quos, tempora, similique, illum molestiae quae illo voluptate cum harum recusandae obcaecati inventore laudantium sapiente?</p>
         <p>Ipsum, est, suscipit, perspiciatis, hic deserunt laboriosam molestias sint sit quibusdam officiis id tempora in eaque nobis cum aspernatur doloremque fuga expedita porro nisi. Excepturi odit adipisci impedit voluptatum molestias!</p>
         <p>Eius, reprehenderit, tempora deserunt dolor itaque eveniet cumque magnam nam sit molestiae atque consectetur perspiciatis praesentium laborum alias nobis tenetur ex incidunt! Porro vero itaque doloremque enim rerum numquam nisi.</p>
-    </body>
+    </body> 
 
 </html>
